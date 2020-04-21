5bbb210e218f5b49c192d2091a563291e2f1be8b
koudai <yamaokoudai33@gmail.com>
2020-04-21 20:01:06 +0900

    create class newsarticle

---
+++ lib/models/newsArticle.dart
@@ -0,0 +1,17 @@
+class NewsArticle {
+  final String title;
+  final String description;
+  final String urlToImage;
+  final String url;
+
+  NewsArticle({this.title, this.description, this.urlToImage, this.url});
+//factoryコンストラクタはインスタンスを手動で生成する
+  factory NewsArticle.formJSON(Map<String, dynamic> json) {
+    return NewsArticle(
+      title: json["title"],
+      description: json["desctiption"],
+      urlToImage:json["urlToImage"],
+      url: json["url"],
+      );
+  }
+}
