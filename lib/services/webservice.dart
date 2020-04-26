bea58872476d48654c6b72979b2e0010d879dbc5
koudai <yamaokoudai33@gmail.com>
2020-04-27 06:03:59 +0900

    create Future

--- lib/services/webservice.dart
+++ lib/services/webservice.dart
@@ -1,23 +1,30 @@
 import 'dart:convert';
-
+import 'package:news_app/utils/constants.dart';
 import 'package:http/http.dart' as http;
 import 'package:news_app/models/newsArticle.dart';
 
 class Webservice {
-  Future<List <NewsArticle>> fetchTopHeadlines() async {
-    String url =
-        "http://newsapi.org/v2/top-headlines?country=jp&apiKey=c9711d7821a24215b2b8b74c90fe743f";
-
-    final response = await http.get(url);
+  Future<List<NewsArticle>> fetchHeadlinesByKeyword(String keyword) async {
+    final response = await http.get(Constants.headlinesFor(keyword));
 
     if (response.statusCode == 200) {
       final result = jsonDecode(response.body);
-
-      Iterable list = result["article"];
-
-      return list.map((article) => NewsArticle.formJSON(article)).toList();
+      Iterable list = result["articles"];
+      return list.map((json) => NewsArticle.fromJSON(json)).toList();
     } else {
-      throw Exception("fialed to get top news");
+      throw Exception("Failed to get news");
+    }
+  }
+
+  Future<List<NewsArticle>> fetchTopHeadlines() async {
+    final response = await http.get(Constants.TOP_HEADLINES_URL);
+
+    if (response.statusCode == 200) {
+      final result = jsonDecode(response.body);
+      Iterable list = result["articles"];
+      return list.map((article) => NewsArticle.fromJSON(article)).toList();
+    } else {
+      throw Exception("Failed to get top news");
     }
   }
 }
